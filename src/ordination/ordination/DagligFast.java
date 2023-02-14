1161d3cba143b044799177cc682c2bd025fe3ef8
MadsOverggard <nissum_10@hotmail.com>
2023-02-14 09:30:27 +0100

    Edited DagligFast.java

--- src/ordination/ordination/DagligFast.java
+++ src/ordination/ordination/DagligFast.java
@@ -1,5 +1,20 @@
 package ordination.ordination;
 
-public class DagligFast {
+public class DagligFast extends Ordination {
+
+    @Override
+    public double samletDosis() {
+        return 0;
+    }
+
+    @Override
+    public double doegnDosis() {
+        return 0;
+    }
+
+    @Override
+    public String getType() {
+        return null;
+    }
     // TODO
 }

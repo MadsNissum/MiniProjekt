7b171f7b105df250a2ffda41a70c3e4f6488a1eb
simon <simon_bruslund@hotmail.com>
2023-02-14 09:48:03 +0100

    Added link to dosis and added method opretDosis in class DagligSkaev

--- src/ordination/ordination/DagligSkaev.java
+++ src/ordination/ordination/DagligSkaev.java
@@ -1,12 +1,37 @@
 package ordination.ordination;
 
+import java.sql.Array;
 import java.time.LocalTime;
+import java.util.ArrayList;
 
-public class DagligSkaev {
+public class DagligSkaev extends Ordination {
     // TODO
 
+    // Link to Dosis
+    private final ArrayList<Dosis> dosisListe = new ArrayList<>();
 
+    public ArrayList<Dosis> getDosisListe() {
+        return new ArrayList<>(dosisListe);
+    }
+
+    // Methods
     public void opretDosis(LocalTime tid, double antal) {
-        // TODO
+        Dosis dosis = new Dosis(tid, antal);
+        dosisListe.add(dosis);
+    }
+
+    @Override
+    public double samletDosis() {
+        return 0;
+    }
+
+    @Override
+    public double doegnDosis() {
+        return 0;
+    }
+
+    @Override
+    public String getType() {
+        return null;
     }
 }

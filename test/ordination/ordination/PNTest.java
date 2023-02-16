7d9eb2dc15d0a48650902ca4f6f8621bf2af0d50
Kamilla <eaakarie@students.eaaa.dk>
2023-02-16 09:45:48 +0100

    OrdinationTest i PNTest

--- test/ordination/ordination/PNTest.java
+++ test/ordination/ordination/PNTest.java
@@ -32,4 +32,48 @@ public class PNTest {
         assertFalse(result);
     }
 
+
+    // Ordination test
+    @Test
+    void TC1_antalDage() {
+        //Arrange
+        Patient patient = new Patient("", "", 10);
+        Laegemiddel laegemiddel = new Laegemiddel("", 1, 1.5, 2, "");
+        pn = new PN(LocalDate.of(2023, 2, 20), LocalDate.of(2023,2,27), patient, laegemiddel, 5);
+
+        //Act
+        int result = pn.antalDage();
+
+        //Assert
+        int forventetAntal = 7;
+    }
+
+    @Test
+    void TC2_antalDage() {
+        Patient patient = new Patient("", "", 10);
+        Laegemiddel laegemiddel = new Laegemiddel("", 1, 1.5, 2, "");
+        pn = new PN(LocalDate.of(2023, 2, 27), LocalDate.of(2023,2,20), patient, laegemiddel, 5);
+
+        //Act
+        int result = pn.antalDage();
+
+        //Assert
+        int forventetAntal = 7;
+    }
+
+    @Test
+    void TC3_antalDage() {
+        Patient patient = new Patient("", "", 10);
+        Laegemiddel laegemiddel = new Laegemiddel("", 1, 1.5, 2, "");
+        pn = new PN(null, LocalDate.of(2023,2,20), patient, laegemiddel, 5);
+
+        //Act
+        Exception exception = assertThrows(RuntimeException.class, ()-> {
+            pn.antalDage();
+        });
+
+        //Assert
+        assertEquals("Cannot invoke \"java.time.temporal.Temporal.until(java.time.temporal.Temporal, java.time.temporal.TemporalUnit)\" because \"temporal1Inclusive\" is null", exception.getMessage());
+    }
+
 }

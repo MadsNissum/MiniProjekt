812238a7f2b58750f0b001135833f5e3a8e4d2d2
MadsOverggard <nissum_10@hotmail.com>
2023-02-16 11:58:49 +0100

    Oprettede alle test til Controller

--- test/ordination/ordination/DagligFastTest.java
+++ test/ordination/ordination/DagligFastTest.java
@@ -1,39 +1,51 @@
 package ordination.ordination;
 
+import ordination.ordination.DagligFast;
+import ordination.ordination.Dosis;
+import ordination.ordination.Patient;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.sql.Array;
-import java.time.LocalDate;
+import java.time.LocalTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DagligFastTest {
 
-    DagligFast dagligFast;
 
-    @BeforeEach
-    void setUp() {
-        Patient jane = new Patient("290395-1922", "Jane", 75.5);
-        Laegemiddel Ibuprofen= new Laegemiddel("Ibuprofen", 2.2, 3.5, 5.0, "stk");
-        double[] doser = new double[]{2, 1, 0, 1};
-        dagligFast = new DagligFast(LocalDate.of(2023,2,1), LocalDate.of(2023, 2, 10), jane, Ibuprofen, doser);
+    private DagligFast dagligFast;
+
+    @Test
+    void TC1_samletDosisTest() {
+        dagligFast = new DagligFast(null, null, new Patient("","",10), null, new double[]{1,0,0,0});
+
+        Dosis[] doses = new Dosis[4];
+
+        doses[0] = new Dosis(LocalTime.of(6, 0), 1);
+        doses[1] = new Dosis(LocalTime.NOON, 0);
+        doses[2] = new Dosis(LocalTime.of(18,0), 0);
+        doses[3] = new Dosis(LocalTime.MIDNIGHT, 0);
+
+        assertEquals(doses[0].getAntal(), dagligFast.getDoser()[0].getAntal());
+        assertEquals(doses[1].getAntal(), dagligFast.getDoser()[1].getAntal());
+        assertEquals(doses[2].getAntal(), dagligFast.getDoser()[2].getAntal());
+        assertEquals(doses[3].getAntal(), dagligFast.getDoser()[3].getAntal());
     }
 
     @Test
-    void samletDosis() {
+    void TC2_samletDosisTest() {
+        dagligFast = new DagligFast(null, null, new Patient("","",10), null, new double[]{-1,0,0,0});
 
-        //Arrange
-        dagligFast.samletDosis();
+        Dosis[] doses = new Dosis[4];
 
+        doses[0] = new Dosis(LocalTime.of(6, 0), 0);
+        doses[1] = new Dosis(LocalTime.NOON, 0);
+        doses[2] = new Dosis(LocalTime.of(18,0), 0);
+        doses[3] = new Dosis(LocalTime.MIDNIGHT, 0);
 
-        //Act
-
-        //Assert
-
-    }
-
-    @Test
-    void doegnDosis() {
+        assertEquals(doses[0].getAntal(), dagligFast.getDoser()[0].getAntal());
+        assertEquals(doses[1].getAntal(), dagligFast.getDoser()[1].getAntal());
+        assertEquals(doses[2].getAntal(), dagligFast.getDoser()[2].getAntal());
+        assertEquals(doses[3].getAntal(), dagligFast.getDoser()[3].getAntal());
     }
 }

bd2611b0d44d024597c73bfe1afacdfdbb9af89d
MadsOverggard <nissum_10@hotmail.com>
2023-02-16 09:29:38 +0100

    Edited small things

--- test/ControllerTest.java
+++ test/ControllerTest.java
@@ -1,10 +1,13 @@
 import ordination.controller.Controller;
-import ordination.ordination.Laegemiddel;
-import ordination.ordination.Patient;
+import ordination.ordination.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ControllerTest {
     private Controller controller;
@@ -15,7 +18,7 @@ public class ControllerTest {
     }
 
     @Test
-    void TC1_anbefaletDosis() {
+    void TC1_anbefaletDosis_Let() {
         Patient patient = new Patient("", "", 10);
         Laegemiddel laegemiddel = new Laegemiddel("", 1, 1.5, 2, "");
 
@@ -25,7 +28,7 @@ public class ControllerTest {
     }
 
     @Test
-    void TC2_anbefaletDosis() {
+    void TC2_anbefaletDosis_normalLavGraense() {
         Patient patient = new Patient("", "", 25);
         Laegemiddel laegemiddel = new Laegemiddel("", 1, 1.5, 2, "");
 
@@ -35,7 +38,7 @@ public class ControllerTest {
     }
 
     @Test
-    void TC3_anbefaletDosis() {
+    void TC3_anbefaletDosis_normalHoejGraense() {
         Patient patient = new Patient("", "", 120);
         Laegemiddel laegemiddel = new Laegemiddel("", 1, 1.5, 2, "");
 
@@ -45,7 +48,7 @@ public class ControllerTest {
     }
 
     @Test
-    void TC4_anbefaletDosis() {
+    void TC4_anbefaletDosis_tung() {
         Patient patient = new Patient("", "", 130);
         Laegemiddel laegemiddel = new Laegemiddel("", 1, 1.5, 2, "");
 
@@ -53,5 +56,84 @@ public class ControllerTest {
 
         assertEquals(260, result);
     }
+
+
+    @Test
+    void TC1_opretPNOrdiantion() {
+        Patient patient = new Patient("", "", 10);
+        Laegemiddel laegemiddel = new Laegemiddel("", 1, 1.5, 2, "");
+
+
+        PN result = controller.opretPNOrdination(LocalDate.of(2023, 1, 1), LocalDate.of(2023, 1, 14), patient, laegemiddel, 10);
+
+        assertEquals(0, result.samletDosis());
+        assertEquals(14, result.antalDage());
+    }
+
+    @Test
+    void TC2_opretPNOrdiantion_exception() {
+        Patient patient = new Patient("", "", 10);
+        Laegemiddel laegemiddel = new Laegemiddel("", 1, 1.5, 2, "");
+
+
+        Exception exception = assertThrows(RuntimeException.class, () -> {
+            controller.opretPNOrdination(LocalDate.of(2023, 1, 14), LocalDate.of(2023, 1, 1), patient, laegemiddel, 10);
+        });
+
+        assertEquals("Slutdato ligger efter startdato", exception.getMessage());
+    }
+
+    @Test
+    void TC1_opretDagligFastOrdiantion() {
+        Patient patient = new Patient("", "", 10);
+        Laegemiddel laegemiddel = new Laegemiddel("", 1, 1.5, 2, "");
+
+        DagligFast result = controller.opretDagligFastOrdination(LocalDate.of(2023, 1, 1), LocalDate.of(2023, 1, 14), patient, laegemiddel, new double[]{1,3,4,0});
+
+        assertEquals(8, result.samletDosis());
+        assertEquals(14, result.antalDage());
+    }
+
+    @Test
+    void TC2_opretDagligFastOrdiantion_exception() {
+        Patient patient = new Patient("", "", 10);
+        Laegemiddel laegemiddel = new Laegemiddel("", 1, 1.5, 2, "");
+
+
+        Exception exception = assertThrows(RuntimeException.class, () -> {
+            controller.opretDagligFastOrdination(LocalDate.of(2023, 1, 14), LocalDate.of(2023, 1, 1), patient, laegemiddel, new double[]{1,3,4,0});
+        });
+
+        assertEquals("Slutdato ligger efter startdato", exception.getMessage());
+    }
+
+    @Test
+    void TC1_opretDagligSkaevOrdiantion() {
+        Patient patient = new Patient("", "", 10);
+        Laegemiddel laegemiddel = new Laegemiddel("", 1, 1.5, 2, "");
+        LocalTime[] klokkeslet = new LocalTime[]{LocalTime.of(12,0), LocalTime.of(18,30)};
+        double[] antalEnheder = new double[]{2,5};
+
+        DagligSkaev result = controller.opretDagligSkaevOrdination(LocalDate.of(2023, 1, 1), LocalDate.of(2023, 1, 14), patient, laegemiddel, klokkeslet, antalEnheder);
+
+        assertEquals(7, result.samletDosis());
+        assertEquals(14, result.antalDage());
+    }
+
+    @Test
+    void TC2_opretDagligSkaevOrdiantion_exception() {
+        Patient patient = new Patient("", "", 10);
+        Laegemiddel laegemiddel = new Laegemiddel("", 1, 1.5, 2, "");
+        LocalTime[] klokkeslet = new LocalTime[]{LocalTime.of(12,0), LocalTime.of(18,30)};
+        double[] antalEnheder = new double[]{2,5};
+
+        Exception exception = assertThrows(RuntimeException.class, () -> {
+            controller.opretDagligSkaevOrdination(LocalDate.of(2023, 1, 14), LocalDate.of(2023, 1, 1), patient, laegemiddel, klokkeslet, antalEnheder);
+        });
+
+        assertEquals("Slutdato ligger efter startdato", exception.getMessage());
+    }
+
+
 
 }

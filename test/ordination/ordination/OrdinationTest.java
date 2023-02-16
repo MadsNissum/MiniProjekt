0d9d593cf551ba81cfeaa31c21239232729e7510
Kamilla <eaakarie@students.eaaa.dk>
2023-02-16 09:08:23 +0100

    OrdinationTest

---
+++ test/ordination/ordination/OrdinationTest.java
@@ -0,0 +1,35 @@
+package ordination.ordination;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class OrdinationTest {
+
+    private Ordination ordination;
+
+    @BeforeEach
+    void setup() {
+
+    }
+
+    @Test
+    void TC1_antalDage() {
+        //Arrange
+        //Act
+        //Assert
+    }
+
+    @Test
+    void TC2_antalDage() {
+        //Arrange
+        //Act
+        //Assert
+    }
+
+    @Test
+    void TC3_antalDage() {
+        //Arrange
+        //Act
+        //Assert
+    }
+}
